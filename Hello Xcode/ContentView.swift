2d9cd37fb1cf56c22aeb106fa88de222359bb091
Ohmeko Ocampo <ohmeko.ocampo@gmail.com>
2025-11-19 22:31:02 -0800

    Created a gradient, edited text, and added a logo.

--- Hello Xcode/ContentView.swift
+++ Hello Xcode/ContentView.swift
@@ -7,15 +7,26 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View { //View is a protocol, you need some certain things for a protocol.
     var body: some View {
-        VStack{
-            Image(systemName:"globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello World!")
+        ZStack{
+            //Background Color
+            LinearGradient(
+                colors: [Color.blue, Color.cyan.opacity(0.5), Color.indigo],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+                .ignoresSafeArea()
+            VStack{ //You have Vstacks, HStack, ZStack
+                Image(systemName:"person.fill") //Image is a view
+                    .imageScale(.large)
+                    .foregroundStyle(.yellow)
+                Text("Hello, Xcode!") //Text is a view
+                    .font(Font.largeTitle)
+                    .fontWeight(.thin)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
